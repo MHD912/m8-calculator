a13bc044a519a04f6f1d1092ec3de1af327a6add
Mohammad Hussein Hamed <hussein.912.workshop@gmail.com>
2023-08-08 14:09:06 +0300

    Initial commit

---
+++ app/src/main/java/mooc/vandy/java4android/calculator/logic/OperationInterface.java
@@ -0,0 +1,5 @@
+package mooc.vandy.java4android.calculator.logic;
+
+public interface OperationInterface {
+    int calculate(int argumentOne, int argumentTwo);
+}
---
+++ app/src/main/java/mooc/vandy/java4android/calculator/logic/Subtract.java
@@ -0,0 +1,12 @@
+package mooc.vandy.java4android.calculator.logic;
+
+/**
+ * Perform the Subtract operation.
+ */
+public class Subtract implements OperationInterface {
+    // TODO - add your solution here.
+    @Override
+    public int calculate(int argumentOne, int argumentTwo) {
+        return 0;
+    }
+}
